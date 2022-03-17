eb6e7a57641f261ec8bf59319bb1e6725e41848a
Mark Rabey <mark@markrabey.com>
2022-03-17 10:12:40 -0400

    update sandbox with aliases

--- sandbox/src/App.tsx
+++ sandbox/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
-import Flag from '@markrabey/react-native-web-flags';
+import Flag from '@markrabey/react-native-flags';
 
 const App = () => {
   return (
