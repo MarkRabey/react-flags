9bcb4f277e5e385d7ec8cbccbca93b1f9c597ab2
Mark Rabey <mark@markrabey.com>
2022-03-18 13:24:38 -0400

    add and update using tests

--- src/components/Flag/__tests__/index.test.tsx
+++ src/components/Flag/__tests__/index.test.tsx
@@ -3,7 +3,30 @@ import renderer from 'react-test-renderer';
 
 import Flag from '../index';
 
-test('Flag', () => {
-  const result = renderer.create(<Flag countryCode="CA" />).toJSON();
-  expect(result).toMatchSnapshot();
+describe('Flag', () => {
+  test('renders correctly', () => {
+    const result = renderer.create(<Flag countryCode="CA" />).toJSON();
+    expect(result).toMatchSnapshot();
+  });
+
+  test('renders flat image correctly', () => {
+    const result = renderer
+      .create(<Flag countryCode="CA" variant="flat" />)
+      .toJSON();
+    expect(result).toMatchSnapshot();
+  });
+
+  test('checks country code for "IN"', () => {
+    const result = renderer
+      .create(<Flag countryCode="IN" variant="flat" />)
+      .toJSON();
+    expect(result).toMatchSnapshot();
+  });
+
+  test('checks country code for "DO"', () => {
+    const result = renderer
+      .create(<Flag countryCode="DO" variant="flat" />)
+      .toJSON();
+    expect(result).toMatchSnapshot();
+  });
 });
