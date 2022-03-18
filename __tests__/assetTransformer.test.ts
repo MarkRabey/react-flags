1ddaae3b1449789b9074cefa60bc066963af031b
Mark Rabey <mark.rabey@bounteous.com>
2022-03-18 16:09:14 -0400

    remove console.log from a test

--- __tests__/assetTransformer.test.ts
+++ __tests__/assetTransformer.test.ts
@@ -3,8 +3,6 @@ import {process} from '../assetsTransformer';
 describe('Jest assetsTransformer', () => {
   test('process() returns expected string value', () => {
     const result = process(null, 'test', null, null);
-    console.log({result});
-
     expect(result).toEqual('module.exports = "test";');
   });
 });
